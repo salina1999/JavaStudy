38660a827b96595a9eccdf9435873489d7835d21
USER <USER@DESKTOP-62QVSFL>
2022-10-31 02:42:08 +0900

    조건문if

---
+++ JavaProject/src/javatutorials/condition/Condition1Demo.java
@@ -0,0 +1,18 @@
+package javatutorials.condition;
+
+public class Condition1Demo {
+
+	public static void main(String[] args) {
+
+		// 조건문
+		
+		if(true) {
+			System.out.println("result : true");
+		}
+		
+		/*
+		 * if(false) { System.out.println("result : true"); }
+		 */
+	}
+
+}
---
+++ JavaProject/src/javatutorials/condition/Condition3Demo.java
@@ -0,0 +1,10 @@
+package javatutorials.condition;
+
+public class Condition3Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
