d60b382f105d7f3a9d08191cecedeb4266aaf02d
USER <USER@DESKTOP-62QVSFL>
2022-11-06 23:53:00 +0900

    반복문 while

---
+++ JavaProject/src/javatutorials/loop/WhileDemo2.java
@@ -0,0 +1,16 @@
+package javatutorials.loop;
+
+public class WhileDemo2 {
+
+	public static void main(String[] args) {
+
+		
+		int i = 0;
+		
+		while(i < 10) {
+			System.out.println("나래뿅" + i + "번째 반복");
+			i++;
+		}
+	}
+
+}
