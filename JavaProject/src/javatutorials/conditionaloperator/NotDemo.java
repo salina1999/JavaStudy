bd2f5ef8226d1d5b1af78b59b414430f3cd75b98
USER <USER@DESKTOP-62QVSFL>
2022-11-06 23:49:03 +0900

    논리연산자 NOT

---
+++ JavaProject/src/javatutorials/conditionaloperator/NotDemo.java
@@ -0,0 +1,14 @@
+package javatutorials.conditionaloperator;
+
+public class NotDemo {
+
+	public static void main(String[] args) {
+
+		if(!true)
+			System.out.println(1);
+		
+		if(!false)
+			System.out.println(2);
+		
+	}
+}
