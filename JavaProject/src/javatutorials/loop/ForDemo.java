e9de588963e56dd72532fe65d90c23206d7ed4cd
USER <USER@DESKTOP-62QVSFL>
2022-11-06 23:55:02 +0900

    반복문 for

---
+++ JavaProject/src/javatutorials/loop/ForDemo.java
@@ -0,0 +1,12 @@
+package javatutorials.loop;
+
+public class ForDemo {
+
+	public static void main(String[] args) {
+
+		for(int i = 5; i < 20; i += 2) {
+			System.out.println("나래반복" + i);
+		}
+	}
+
+}
